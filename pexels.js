39c91054a029d756292df383acbd11df80fa25fc
Yogesh889 <8058yogesh@gmail.com>
2023-10-11 13:28:17 +0530

    commit

--- pexels.js
+++ pexels.js
@@ -1,53 +1,63 @@
-// // import { createClient } from 'pexels';
-
-// // const client = createClient('IhJzYg9chacPhZdCqNm5z66aMZRHRdgNOd8vzbA9BidMMjHCIfnJGTmg');
-
-// // All requests made with the client will be authenticated
-
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'IhJzYg9chacPhZdCqNm5z66aMZRHRdgNOd8vzbA9BidMMjHCIfnJGTmg',
-//         'X-RapidAPI-Host': 'https://api.pexels.com/v1/curated?page=2&per_page=40'
-
-//     }
-// };
+var pagurl = 'https://api.pexels.com/v1/curated?page=2&per_page=40';
 
 
-// fetch('https://api.pexels.com/v1/curated?page=2&per_page=40', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-
-const apiKey = 'IhJzYg9chacPhZdCqNm5z66aMZRHRdgNOd8vzbA9BidMMjHCIfnJGTmg';
-const query = 'cat';
-const url = `https://api.pexels.com/v1/search?query=${query}&per_page=10`;
-
-fetch(url, {
+const options = {
+  method: 'GET',
   headers: {
-    Authorization: apiKey,
-  },
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    let ihtml = ""
-    for (const item of data.photos) {
-          ihtml += `
+    Authorization: 'IhJzYg9chacPhZdCqNm5z66aMZRHRdgNOd8vzbA9BidMMjHCIfnJGTmg',
+    'X-RapidAPI-Key': '6c5fdf6306mshe8cce73b2aa3666p1f48ccjsn4b22b91415e3',
+    'X-RapidAPI-Host': 'PexelsdimasV1.p.rapidapi.com'
+  }
+};
+
+let result; // declare the result variable here
+
+let pexels = async (x) => {
+  var url = 'https://pexelsdimasv1.p.rapidapi.com/v1/search?query='+x+'&locale=en-US&per_page=15&page=1';
+  try {
+    const response = await fetch(url, options);
+    result = await response.json(); // assign the result inside the try block
+    console.log(result);
+
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(result); // result is accessible here
+  let ihtml = ""
+  for (const item of result.photos) {
+    ihtml += `
             <div class="card" style="width: 18rem;">
             <img src="${item.src.small}" class="card-img-top" alt="...">
               <div class="card-body">
-                <p class="card-title">${item.alt}</p>
-                <p class="card-title">${item.photographer}</p>
+              <p class="card-title">${item.alt}</p>
+              <p class="card-title">${item.photographer}</p>
                 <a id="button" target="blank" href="${item.url}" class="btn btn-primary my-6">Visit site</a>
               </div>
             </div>
           `
-    }
-    cardContainer.innerHTML = ihtml
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  }
+  cardContainer.innerHTML = ihtml
+}
 
+
+
+var myFunction = (x) => {
+
+
+    var x = document.getElementById("mySearch").value;
+    console.log(x)
+    document.getElementById("demo").innerHTML = x;
+    if (x != "") {
+      console.log(x)
+      pexels(x)
+    }
+    else{
+      var url = 'https://pexelsdimasv1.p.rapidapi.com/v1/search?query=ocean&locale=en-US&per_page=15&page=1';
+      pexels(url)
+    }
+  }
+
+  var nextPage = () => {
+    
+  }
--- photos.js
+++ photos.js
@@ -1,38 +1,58 @@
-const url = 'https://stock-photos-and-videos.p.rapidapi.com/api/suggestions';
-console.log(url)
+const url = 'https://stock-photos-and-videos.p.rapidapi.com/api/search?query='+x;
+
 const options = {
-    method: 'POST',
+    method: 'GET',
     headers: {
         'content-type': 'application/x-www-form-urlencoded',
         'X-RapidAPI-Key': 'f1aa506a2fmsh320dcd451cede70p17ae28jsnb96f61835673',
         'X-RapidAPI-Host': 'stock-photos-and-videos.p.rapidapi.com'
     },
-    body: new URLSearchParams({ term: 'technology' })
+
 };
-async function stockPhotos() {
+
+
+let photos = async (x) => {
+    const url = new URLSearchParams({term: 'technology'})
+    console.log(url)
+
+    // const url = 'https://stock-photos-and-videos.p.rapidapi.com/api/suggestions'
+
+
     try {
         const response = await fetch(url, options);
         const result = await response.json();
         console.log(result);
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
+
+    let ihtml = ""
+    for (const item of result.photos) {
+        ihtml += `
+            <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="...">
+              <div class="card-body">
+              <p class="card-title">${item.slug}</p>
+              <p class="card-title">${item.title}</p>
+                <a id="button" target="blank" href="${item.updated_at}" class="btn btn-primary my-6">Visit site</a>
+              </div>
+            </div>
+          `
+    }
+    cardContainer.innerHTML = ihtml
 
 }
 
-stockPhotos()
-    .then(data => {
+let stockPhotos = () => {
+    let x = document.getElementById("search").value;
+    console.log(x)
+    document.getElementById("demo").innerHTML = x;
+    if (x != "") {
+        console.log(x)
+        photos()
+    }
+    else {
+        photos()
+    }
+}
 
-        let ihtml = ""
-        for (const item of data) {
-            ihtml += `
-              <div class="card" style="width: 18rem;">
-              <img src="${item.id}" class="card-img-top" alt="...">
-              </div>
-            `
-            console.log(data)
-        };
-
-        cardContainer.innerHTML = ihtml 
-
-    });
